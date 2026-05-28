124ad3a9454e08dd346a51908baf42228a9fb8cb
Francesco Falessi <francesco.falessi@skywarder.eu>
2026-05-29 00:40:44 +0200

    [main] Parsed data organized in a single database separated in different seasons automatically

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -5,10 +5,12 @@ This script performs statistical analysis on the imported player data.
 ----------------------------------------------------------------------
 """
 
+
 # PACKS
 import os   # for file handling
 import pandas as pd   # for data analysis
 from dataclasses import dataclass   # for creating data structures
+
 
 # CLASSES
 @dataclass
@@ -30,6 +32,7 @@ class player:
     yellow_cards: int
     red_cards: int
     own_goals: int
+
 
 # FUNCTIONS
 def importer_parser(file_path: str) -> list[player]:
@@ -53,35 +56,56 @@ def importer_parser(file_path: str) -> list[player]:
     # Remove eventual leading and trailing whitespaces
     df.columns = df.columns.str.strip()
 
-    # Create a list of players
+    # Create a list of players and fill it with a for loop
     players = []
-    for index, row in df.iterrows():
+    for _, row in df.iterrows():
         player_obj = player(
-            position = str(row.get('Ruolo', '')).strip(),
-            position_mantra = str(row.get('Ruolo mantra', '')).strip(),
+            position = str(row.get('R', '')).strip(),
+            position_mantra = str(row.get('Rm', '')).strip(),
             name = str(row.get('Nome', '')).strip(),
             team = str(row.get('Squadra', '')).strip(),
-            games_played = int(row.get('Partite a voto', 0)),
-            average_mark = float(row.get('Media voto', 0.0)),
-            average_fantamark = float(row.get('Fanta media', 0.0)),
-            goals = int(row.get('Gol fatti', 0)),
-            goals_conceded = int(row.get('Gol subiti', 0)),
-            penalties_saved = int(row.get('Rigori parati', 0)),
-            penalties_taken = int(row.get('Rigori calciati', 0)),
-            penalties_scored = int(row.get('Rigori segnati', 0)),
-            penalties_missed = int(row.get('Rigori sbagliati', 0)),
-            assists = int(row.get('Assist', 0)),
-            yellow_cards = int(row.get('Ammonizioni', 0)),
-            red_cards = int(row.get('Espulsioni', 0)),
-            own_goals = int(row.get('Autogol', 0))
+            games_played = int(row.get('Pv', 0)),
+            average_mark = float(row.get('Mv', 0.0)),
+            average_fantamark = float(row.get('Fm', 0.0)),
+            goals = int(row.get('Gf', 0)),
+            goals_conceded = int(row.get('Gs', 0)),
+            penalties_saved = int(row.get('Rp', 0)),
+            penalties_taken = int(row.get('Rc', 0)),
+            penalties_scored = int(row.get('R+', 0)),
+            penalties_missed = int(row.get('R-', 0)),
+            assists = int(row.get('Ass', 0)),
+            yellow_cards = int(row.get('Amm', 0)),
+            red_cards = int(row.get('Esp', 0)),
+            own_goals = int(row.get('Au', 0))
         )
-        players.append(player_obj)  # Append the player object to the list of players
+        players.append(player_obj)  # Append new element to the list of players
 
     return players
 
+
 # MAIN
 if __name__ == "__main__":
-    # Example usage
-    file_path = 'Data/Statistiche_Fantacalcio_Stagione_2025_26.csv'
-    players_list = importer_parser(file_path)
-    print(players_list[156].name)
+    
+    # Folder where the data is stored
+    folder_name = "Data"
+
+    # Dictionary to store multiple seasons
+    database = {}
+
+    # Check if the folder exists
+    if os.path.exists(folder_name):
+
+        # Loop through all the files in the folder
+        for name_file in os.listdir(folder_name):
+
+            # Check if the file is valid
+            if name_file.endswith('.csv'):
+
+                file_path = os.path.join(folder_name, name_file)
+                season_name = name_file.replace('Statistiche_Fantacalcio_Stagione_', '').replace('.csv', '')
+
+                # Import the data and store it in the database
+                database[season_name] = importer_parser(file_path)
+
+    else:
+        raise FileNotFoundError(f"The folder {folder_name} does not exist.")    # If the folder does not exist, raise an error
